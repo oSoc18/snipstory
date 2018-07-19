4390822f51a4d889d4d48679f88c78c4ea02d4b6
azahmed <azahmed096@gmail.com>
2018-07-19 09:48:53 +0200

    add delete confirm

--- src/components/ModuleEditCard/ModuleEditCard.js
+++ src/components/ModuleEditCard/ModuleEditCard.js
@@ -20,7 +20,10 @@ export default class ModuleEditCard extends React.Component {
         { text }
       </div>
       <div className="module-button-container">
-        <button disabled={deleteDisabled} className="module-delete" onClick={(e) => onDelete(e)}>delete</button>
+        <button disabled={deleteDisabled} className="module-delete" onClick={(e) => {
+          if (window.confirm("This is IRREVERSIBLE!"))
+            onDelete(e);
+        }}>delete</button>
         <button disabled={orderDisabled} className="module-up" onClick={(e) => onOrderUp(e)}>Up</button>
         <button disabled={orderDisabled} className="module-down" onClick={(e) => onOrderDown(e)}>Down</button>
       </div>
