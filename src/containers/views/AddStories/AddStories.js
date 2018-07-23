18a9b045e3f7d3109b9480d4a601034f3bdbb503
Bruce Boelens <bruceboelens@outlook.com>
2018-07-23 16:12:51 +0200

    Add a profile pic upload box

--- src/containers/views/AddStories/AddStories.js
+++ src/containers/views/AddStories/AddStories.js
@@ -7,14 +7,25 @@ import Footer from '../../../components/footer/Footer';
 
 import { Link } from 'react-router-dom';
 import { Field, reduxForm, SubmissionError } from 'redux-form';
-import {firebaseAuth, googleAuthProvider, firebaseDatabase} from '../../../helpers/firebase';
+import {firebaseAuth, googleAuthProvider, firebaseDatabase, firebaseStorage} from '../../../helpers/firebase';
+import moment from 'moment';
+import FileField from '../../../components/filefield/FileField';
 import FormField from '../../../components/form/FormField';
 import './AddStories.css'
 
+// class reduxTags extends React.Compenent {
+//     render() {
+//         return (
+//             <SmallButton type="button" className={value ? 'activeButton': ''} onClick={(e) => this.props.onChange(!this.props.input.value)} value={this.props.input.value}>{this.props.tag}</SmallButton>
+//         )
+//     }
+//
+// }
+
 class AddStories extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             id:"",
             title: "",
@@ -29,33 +40,66 @@ class AddStories extends React.Component {
             link:"",
             nationality:"",
             tags: {
-                poperinge: false,
-                brugge: false,
-                ieper: false,
-                food: false,
-                sport: false,
-                transportation: false
+                locations: {
+                    poperinge: false,
+                    brugge: false,
+                    ieper: false
+                },
+                categories: {
+                    food: false,
+                    sport: false,
+                    transportation: false
+                }
             }
         }
     }
 
     handleTag(e) {
         e.preventDefault;
-        console.log(e.target.value);
 
-        let tags = Object.keys(this.state.tags);
         let newState = Object.assign({}, this.state);
-        for(let i = 0; i < tags.length; i++) {
 
-            if(e.target.value === tags[i]) {
-                let tag = tags[i];
-                newState.tags[e.target.value] = !newState.tags[e.target.value]
-                this.setState(newState)
+        console.log(e.target.value)
 
-            }
-        }
+        Object.entries(this.state.tags).map(([child,tagsOfChild]) => {
+                Object.entries(tagsOfChild).map(([tag,value]) => {
+                    console.log(tag)
+                    if(e.target.value === tag) {
+                        console.log("yes")
+                        newState.tags[e.target.value] = !newState.tags[e.target.value]
+                        this.setState(newState)
+                    }
+
+                })
+            })
+
+        // let tags = Object.entries(this.state.tags);
+        // let newState = Object.assign({}, this.state);
+        // for(let i = 0; i < tags.length; i++) {
+        //
+        //     for(let j = 0; j < tags[i][1].length; j++){
+        //         console.log(tags[i][1])
+        //
+        //         if(e.target.value === tags[i][1][j]) {
+        //             let tag = tags[i][1][j];
+        //             newState.tags[e.target.value] = !newState.tags[e.target.value]
+        //             this.setState(newState)
+        //         }
+        //
+        //     }
+        // }
         console.log(this.state.tags);
         }
+
+    testMethod() {
+        console.log(Object.entries(this.state.tags).map(([key,value]) => {
+            return (
+                <SmallButton>{value}</SmallButton>
+            )
+
+
+        }))
+    }
 
 
     render() {
@@ -64,6 +108,7 @@ class AddStories extends React.Component {
             submitting,
             user,
             history,
+            handleChange,
             logout
         } = this.props;
 
@@ -74,36 +119,59 @@ class AddStories extends React.Component {
             <Navbar />
             <h1>Voeg een verhaal toe</h1>
 
-            <form onSubmit={this.props.handleSubmit(({id,thirdYear,fourthYear,fifthYear,sixthYear,firstYearSecondary,secondYearSecondary,...fields,...props}) => {
+            <form onSubmit={this.props.handleSubmit(({id,thirdYear,fourthYear,fifthYear,sixthYear,firstYearSecondary,secondYearSecondary,profileImage,...fields,...props}) => {
+                let name = ['profileimage'];
+
+                let imagePromise = Promise.all([profileImage].map(
+                  (file, index) => {
+                    return firebaseStorage()
+                    .ref()
+                    .child(user.uid)
+                    .child("story")
+                    .child(moment().format('YYYYMMDD_hhmmss'))
+                    .child(name[index])
+                    .put(file);
+                })
+               );
+
+
+               let dbPromise = imagePromise.then(
+               (tasks) /* <- type = UploadTaskSnapshot[] */ => {
+                let urlArray = tasks.map(t => t.metadata.downloadURLs[0]);
                 let o = firebaseDatabase.ref('stories/');
-                o.child(id).child("general").set({
+                o.child(id).set({
                         id,
-                        userId: user.uid,
-                        schoolYear: {
-                            thirdYear: thirdYear || false,
-                            fourthYear: fourthYear || false,
-                            fifthYear: fifthYear || false,
-                            sixthYear: sixthYear || false,
-                            firstYearSecondary: firstYearSecondary || false,
-                            secondYearSecondary: secondYearSecondary || false
-                        },
-                        tags: {
-                            locations: {
-                                poperinge: this.state.tags.poperinge,
-                                brugge: this.state.tags.brugge,
-                                ieper: this.state.tags.ieper
+                        general: {
+                            userId: user.uid,
+                            schoolYear: {
+                                thirdYear: thirdYear || false,
+                                fourthYear: fourthYear || false,
+                                fifthYear: fifthYear || false,
+                                sixthYear: sixthYear || false,
+                                firstYearSecondary: firstYearSecondary || false,
+                                secondYearSecondary: secondYearSecondary || false
                             },
-                            categories: {
-                                food: this.state.tags.food,
-                                sport: this.state.tags.sport,
-                                transportation: this.state.tags.transportation
-                            }
-                        },
-                        ...fields
+                            tags: {
+                                locations: {
+                                    poperinge: this.state.tags.locations.poperinge,
+                                    brugge: this.state.tags.locations.brugge,
+                                    ieper: this.state.tags.locations.ieper
+                                },
+                                categories: {
+                                    food: this.state.tags.categories.food,
+                                    sport: this.state.tags.categories.sport,
+                                    transportation: this.state.tags.categories.transportation
+                                }
+                            },
+                            profilePicture: urlArray,
+                            ...fields
+                        }
+                    })
                 })
                 .then(() => history.push("/teacher/dashboardstorylist"))
-            })
-            }>
+
+            }
+            )}>
             <div className="general-container">
                 <div className="form-box container">
                     <h1>Voeg een verhaal toe</h1>
@@ -265,6 +333,16 @@ class AddStories extends React.Component {
                             required
                         />
                         </div>
+                        <div>
+                          <Field
+                            name="profileImage"
+                            type="file"
+                            label="Voeg een foto toe"
+                            component={FileField}
+                            onChange={handleChange}
+                            required
+                          />
+                        </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6">
@@ -288,31 +366,25 @@ class AddStories extends React.Component {
                     <div>
                     <div className="row">Belangrijke locaties</div>
                     <div className="row">
-                        <div className="col-md-4">
-                            <SmallButton type="button" className={this.state.tags.brugge ? 'activeButton': ''} onClick={(e) => this.handleTag(e)} value="brugge">Brugge</SmallButton>
-                        </div>
-                        <div className="col-md-4">
-                            <SmallButton type="button" className={this.state.tags.ieper ? 'activeButton': ''} onClick={(e) => this.handleTag(e)} value="ieper">Ieper</SmallButton>
-                        </div>
-                        <div className="col-md-4">
-                            <SmallButton type="button" className={this.state.tags.poperinge ? 'activeButton': ''} onClick={(e) => this.handleTag(e)} value="poperinge">Poperinge</SmallButton>
-                        </div>
+
+                    {this.testMethod()}
+
+                    {Object.entries(this.state.tags).map(([child,tagsOfChild]) => {
+                            return Object.entries(tagsOfChild).map(([tag,value]) => {
+                                return (
+                                    <div key={tag}>
+                                        <SmallButton type="button" className={value ? 'activeButton': ''} onClick={(e) => this.handleTag(e)} value={tag}>{tag}</SmallButton>
+                                    </div>
+                                )
+                            })
+                        }
+                    )
+                }
+
                     </div>
-                    </div>
-                    <div>
-                    <div className="row">Categorie</div>
-                    <div className="row">
-                        <div className="col-md-4">
-                            <SmallButton type="button" className={this.state.tags.food ? 'activeButton': ''} onClick={(e) => this.handleTag(e)} value="food">Voedsel</SmallButton>
-                        </div>
-                        <div className="col-md-4">
-                            <SmallButton type="button" className={this.state.tags.sport ? 'activeButton': ''} onClick={(e) => this.handleTag(e)} value="sport">Sport</SmallButton>
-                        </div>
-                        <div className="col-md-4">
-                            <SmallButton type="button" className={this.state.tags.transportation ? 'activeButton': ''} onClick={(e) => this.handleTag(e)} value="transportation">Transport</SmallButton>
-                        </div>
-                    </div>
-                    </div>
+                </div>
+
+
 
 
                     <Button className="submit_button" type="submit" disabled={pristine || submitting}>Voeg een verhaal toe</Button>
