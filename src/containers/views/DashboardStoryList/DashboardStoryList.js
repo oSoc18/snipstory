d3e4745d23a02d69a6b94d6960d5059a84568e5f
Pol Labaut <pol.labaut@gmail.com>
2018-07-24 09:25:14 +0200

    merge 2

--- src/containers/views/DashboardStoryList/DashboardStoryList.js
+++ src/containers/views/DashboardStoryList/DashboardStoryList.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import ButtonSmall from '../../../components/button-small/ButtonSmall';
-=======
 import Button from '../../../components/button/Button';
->>>>>>> develop
 import Navbar from '../../../components/nav/Navbar';
 import Footer from '../../../components/footer/Footer';
 import Spinner from '../../../components/spinner/Spinner';
@@ -74,28 +70,6 @@ class DashboardStoryList extends React.Component {
                                 className="storyCards"
                                 id = {story.general.id}
                             >
-<<<<<<< HEAD
-                                <div className="">
-                                    <h2>{story.general.title}</h2>
-                                    <p>{story.general.summary}</p>
-                                </div>
-
-                                <div className="flex">
-                                <ButtonSmall
-                                onClick={(e) => {
-                                    this.props.fetchStory(story.id)
-                                    .then(() => history.push(`/dashboardstorylist/${story.id}/edit`));
-                                }}
-                                >Aanpassen</ButtonSmall>
-                                <ButtonSmall onClick={(e) => {
-                                    if (window.confirm('Are you sure you wish to delete this item?')) deleteSt(story) } }>
-                                Verwijder
-                                </ButtonSmall>
-                                </div>
-
-                                <div className="flex">
-                                <ButtonSmall>Maak onzichtbaar</ButtonSmall>
-=======
                                 <div
                                     className="flex"
                                 >
@@ -124,7 +98,6 @@ class DashboardStoryList extends React.Component {
                                     <Button size="small" onClick={(e) => this.handleVisibility(e)}>Maak onzichtbaar</Button>
                                     </div>
 
->>>>>>> develop
                                 </div>
                             </div>
                         )
