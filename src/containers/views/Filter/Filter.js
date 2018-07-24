c2e6d5bbc3ecd2e53f71fccc6eba7570b84193fe
Pol Labaut <pol.labaut@gmail.com>
2018-07-24 11:30:34 +0200

    Bootstrap base

--- src/containers/views/Filter/Filter.js
+++ src/containers/views/Filter/Filter.js
@@ -5,7 +5,9 @@ import Spinner from '../../../components/spinner/Spinner';
 import { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Filter.css';
-
+import Button from '../../../components/button/Button';
+import Navbar from '../../../components/nav/Navbar';
+import Footer from '../../../components/footer/Footer';
 
 class Filter extends React.Component {
     componentWillMount() {
@@ -26,24 +28,46 @@ class Filter extends React.Component {
             return <Spinner page />
         }
 
-        return <div className="filter-root">
-            <div className="range-container">
-                {this.state.yearRange.join("-")}
-                <Range
-                    max={maxYear}
-                    min={minYear}
-                    allowCross={false}
-                    value={this.state.yearRange}
-                    onChange={yearRange => this.setState({yearRange})}
-                    onAfterChange={setYearRange}
-                />
-            </div>
-
-            {filteredStories.map(story => {
-                return <div key={story.id || story.general.id || story.general.title}>
-                <h2>{story.general.title} ({story.general.dayOfBirth})</h2>
+        return <div className="page">
+        <Navbar />
+        <div className="filter-root container">
+            <div className="row filters-div">
+                <div className="col">
+                    <p>Filters :</p>
                 </div>
-            })}
+                <div className="col">
+                    <Button size="small" inverted="true">Partners</Button>
+                </div>
+                <div className="col">
+                    <Button size="small" inverted="true">Jaar</Button>
+                </div>
+                <div className="col">
+                    <Button size="small" inverted="true">Skills</Button>
+                </div>
+                <div className="col">
+                    <div className="range-container">
+                        {this.state.yearRange.join("-")}
+                        <Range
+                            max={maxYear}
+                            min={minYear}
+                            allowCross={false}
+                            value={this.state.yearRange}
+                            onChange={yearRange => this.setState({yearRange})}
+                            onAfterChange={setYearRange}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="row cards-div">
+                {filteredStories.map(story => {
+                                return <div key={story.id || story.general.id || story.general.title}>
+                                <h2>{story.general.title} ({story.general.dayOfBirth})</h2>
+                                </div>
+                            })}
+            </div>
+            </div>
+            
+        <Footer />
         </div>
     }
 }
