73bf253e0bbfda259b1601e7ba0d840cb6ba8852
Miet Claes <miet@miet.be>
2018-07-25 21:40:52 +0200

    Add gh-pages template

--- src/components/modules/partials/Marker.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Marker = props => <div>Marker</div>;
-
-export default Marker;
--- src/containers/views/CreateStory.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class CreateStory extends React.Component {
-  render() {
-    return <div>Create a story</div>;
-  }
-}
-
-export default CreateStory;
