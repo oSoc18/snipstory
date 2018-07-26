0ae5a73f6c927c6c48ceea4ee93714c1931b6ae4
Pol Labaut <pol.labaut@gmail.com>
2018-07-26 14:14:45 +0200

    fix white spaces in story preview

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -57,7 +57,9 @@ return <div className="cards">
           <div className="info">
             <div className="container">
                 <div className="row">
-                    <h1 className="col-10">{title}</h1>
+                    <div className="col-10">
+                        <h1>{title}</h1>
+                    </div>
                     <div className="difficulty col-2">
                         💪&nbsp;{difficulty}
                     </div>
